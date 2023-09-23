e94c3f6d83862e44ba933b70be9558ad6fa81bcc
djserega <galyuk.sergey@gmail.com>
2023-09-23 11:09:20 +0300

    required prop. added: enableBatchUnload, packetSendCount

--- src/RegistrationLog1CToElasticSearch/Models/MainConfig.cs
+++ src/RegistrationLog1CToElasticSearch/Models/MainConfig.cs
@@ -5,42 +5,46 @@ namespace RegistrationLog1CToElasticSearch.Models
     public class MainConfig
     {
         [JsonPropertyName("main")]
-        public Main Main { get; set; }
+        public required Main Main { get; set; }
         [JsonPropertyName("elasticsearch")]
-        public Elasticsearch Elasticsearch { get; set; }
+        public required Elasticsearch Elasticsearch { get; set; }
         [JsonPropertyName("sqlite")]
-        public SQLite SQLite { get; set; }
+        public required SQLite SQLite { get; set; }
     }
 
     public class Main
     {
         [JsonPropertyName("takeElements")]
-        public int TakeElements { get; set; }
+        public required int TakeElements { get; set; }
         [JsonPropertyName("timeoutSeconds")]
-        public int TimeoutSeconds { get; set; }
+        public required int TimeoutSeconds { get; set; }
+        [JsonPropertyName("enableBatchUnload")]
+        public required bool EnableBatchUnload { get; set; }
+	    [JsonPropertyName("packetSendCount")]
+        public required int PacketSendCount { get; set; }
     }
 
     public class Elasticsearch
     {
         [JsonPropertyName("indexName")]
-        public string IndexName { get; set; }
+        public required string IndexName { get; set; }
         [JsonPropertyName("indexFormat")]
-        public string IndexFormat { get; set; }
+        public required string IndexFormat { get; set; }
         [JsonPropertyName("uri")]
-        public string Uri { get; set; }
+        public required string Uri { get; set; }
         [JsonPropertyName("login")]
-        public string Login { get; set; }
+        public required string Login { get; set; }
         [JsonPropertyName("password")]
-        public string Password { get; set; }
+        public required string Password { get; set; }
     }
 
     public class SQLite
     {
         [JsonPropertyName("logpath")]
-        public string LogPath { get; set; }
+        public required string LogPath { get; set; }
         [JsonPropertyName("dateFrom")]
-        public DateTime DateFrom { get; set; }
+        public required DateTime DateFrom { get; set; }
         [JsonPropertyName("rowIdFrom")]
-        public long RowIdFrom { get; set; }
+        public required long RowIdFrom { get; set; }
     }
 }
